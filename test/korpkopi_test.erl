55ba5d581c4e3de57e533f77a0692f72c21e04e9
Tradunsky <Tradunsky@users.noreply.github.com>
2016-10-29 19:31:25 +0300

    Update korpkopi_test.erl

--- test/korpkopi_test.erl
+++ test/korpkopi_test.erl
@@ -7,12 +7,12 @@
 -include_lib("etest_http/include/etest_http.hrl").
 
 test_the_main_page_is_available() ->
-	Response = ?perform_get("http://localhost:8080/korpkopi"),
+	Response = ?perform_get("http://localhost:8080/be"),
     ?assert_status(200, Response),
     %?assert_body_contains("<ANSWER>main page</ANSWER>", Response),
     ?assert_body("<ANSWER>main page</ANSWER>", Response).
 
 %test_the_otp_is_sent() ->
-%	Response = ?perform_post("http://localhost:8080/phoneAuth?phone=%2B380635975404"),
+%	Response = ?perform_post("http://localhost:8080/be/auth?phone=%2B102"),
 %	?assert_status(200, Response),
 %	?assert_body_contains("<OTP>otp sended successful</OTP>", Response).
